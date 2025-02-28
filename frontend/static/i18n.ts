5cc84685a2d873e788ab78301daafcff03c0c4da
TGRISSE <t.grisse@web.de>
2025-02-28 13:58:33 +0100

    Update i18n.ts
    Added German language support

--- frontend/static/i18n.ts
+++ frontend/static/i18n.ts
@@ -6,6 +6,7 @@ import { getHiddenValue } from './redux/reducers/utils';
 import cn from './translations/cn.json';
 import en from './translations/en.json';
 import pt from './translations/pt.json';
+import de from './translations/de.json'; ##added by tgrisse
 
 const language = getHiddenValue('language') || 'en';
 
